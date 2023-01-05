5e96b8f0ee7def2a352a4f1851b5f71a199233e0
Alex <61790982+BigEmster@users.noreply.github.com>
2023-01-05 10:55:03 -0600

    Create main.js
    Used exclusively for auto-login at this time

---
+++ main.js
@@ -0,0 +1,22 @@
+function checkLoggedIn() {
+  if (getCookie("nick") != "") {
+    document.getElementsByClassName("nick").innerHTML = getCookie("nick");
+  } else {
+    document.getElementsByClassName("dependsOnNick").style.display = "none";
+  }
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
